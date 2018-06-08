e7161ac24632404b9d9b42ac1db6b62d6896655c
Crystopher Glodzienski Cardoso <crystopher12@gmail.com>
2018-06-08 20:15:36 -0300

    Commit inicial do projeto

---
+++ ControleAtividades/Controllers/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Modelos;
+using System.Data.Entity;
+
+namespace Controllers.DAL
+{
+    class Contexto : DbContext
+    {
+        public Contexto() : base("strConn")
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Atividade> Atividades { get; set; }
+    }
+}
